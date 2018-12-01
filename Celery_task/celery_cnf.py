ddbdab0c49bedac93d57a1482554b9eb5d0cef08
pqGC <1371827147@qq.com>
2018-12-01 14:46:17 +0800

    add RabbitMQ

--- Celery_task/celery_cnf.py
+++ Celery_task/celery_cnf.py
@@ -2,11 +2,23 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 from celery.schedules import crontab
 
+
+"""
+启动Celery命令： celery -A Celery_task.celery_cnf worker -l info -P eventlet
+启动beat定时任务：celery beat -A Celery_task.celery_cnf -l info
+"""
+
+
 # 服务器
 # app = Celery('Scheduler', broker='redis://localhost:6379/0',
 #              backend='redis://localhost:6379/1', include=['Scheduler.tasks'])
-# 本机
-app = Celery('Celery_task', broker='redis://127.0.0.1:6379/0', backend='redis://127.0.0.1:6379/1',
+# 本机 Redis
+# app = Celery('Celery_task', broker='redis://127.0.0.1:6379/0', backend='redis://127.0.0.1:6379/1',
+#              include=['Celery_task.tasks'])
+# 本机 RabbitMQ
+app = Celery('Celery_task',
+             broker='amqp://test:123456@127.0.0.1:5672/',
+             backend='amqp://test:123456@127.0.0.1:5672/',
              include=['Celery_task.tasks'])
 
 
@@ -15,6 +27,15 @@ app.conf.beat_schedule = {
         'task': 'Celery_task.tasks.add',
         'schedule': crontab(minute='*/1'),
         'args': (5, 6)
+    },
+}
+
+
+app.conf.beat_schedule = {
+    'RabbitMQ-at-noon-execute-hour': {
+        'task': 'Celery_task.tasks.RabbitMQ_test',
+        'schedule': crontab(minute='*/1'),
+        'args': ()
     },
 }
 app.conf.timezone = 'UTC'
--- Celery_task/tasks.py
+++ Celery_task/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from Celery_task.celery_cnf import app
 import time
+import pika
 
 
 # 爬虫相关
@@ -13,3 +14,27 @@ def spider(*args):
 def add(x, y):
     print('x + y = ', x+y)
     return x + y
+
+
+@app.task
+def RabbitMQ_test():
+    """
+    需要启动RabbitMQ项目的recv_msg3.py
+    :return:
+    """
+    creds_broker = pika.PlainCredentials("test", "123456")
+    # 定义连接池
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='127.0.0.1', port=5672, credentials=creds_broker))
+    # 创建一个消息通道
+    channel = connection.channel()
+    # 创建一个消息队列，消息队列的名称为task_queue，
+    # 这里的第二个参数durable，是对消息队列的设置，
+    # 使得RabbitMQ在发生异常退出时发送的消息不会被丢失，该消息会被发送给其他消费者
+    channel.queue_declare(queue='mytest')
+    for i in range(10):
+        channel.basic_publish(exchange='', routing_key='mytest', body=str(i),
+                              # 注意当未定义exchange时，routing_key需和queue的值保持一致
+                              properties=pika.BasicProperties(delivery_mode=2))  # mode>=2表示消息的持久化
+        print('send success msg to rabbitmq   msg--->{}'.format(i))
+    connection.close()  # 关闭连接
